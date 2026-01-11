0f6f4557d4fc44fce6135b5f807ba15a56280647
Ivan <astanin.i.a@gmail.com>
2026-01-11 13:49:51 +0100

    blinn-phong lighting

--- assets/shaders/instanced/fragment.glsl
+++ assets/shaders/instanced/fragment.glsl
@@ -37,8 +37,8 @@ vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
     // diffuse shading
     float diff = max(dot(normal, lightDir), 0.0);
     // specular shading
-    vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    float spec = pow(max(dot(normal, halfwayDir), 0.0), material.shininess);
     // combine results
     vec3 ambient  = light.ambient  * texture(material.diffuseTexture, fs_in.vs_texcoord).rgb;
     vec3 diffuse  = light.diffuse  * diff * texture(material.diffuseTexture, fs_in.vs_texcoord).rgb;
