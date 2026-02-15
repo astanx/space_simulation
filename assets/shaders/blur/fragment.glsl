eb39ab98192da8e5b51f8a65596ab90040a1628c
Ivan <astanin.i.a@gmail.com>
2026-02-15 18:23:32 +0100

    blur algorithm optimization

--- assets/shaders/blur/fragment.glsl
+++ assets/shaders/blur/fragment.glsl
@@ -7,27 +7,27 @@ in vec2 vs_texcoord;
 uniform sampler2D image;
   
 uniform bool horizontal;
-uniform float weight[5] = float[] (0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216);
+
+uniform float offset[3] = float[](0.0, 1.3846153846, 3.2307692308);
+uniform float weight[3] = float[](0.2270270270, 0.3162162162, 0.0702702703);
 
 void main()
 {             
   vec2 tex_offset = 1.0 / textureSize(image, 0); // gets size of single texel
   vec3 result = texture(image, vs_texcoord).rgb * weight[0]; // current fragment's contribution
-  if(horizontal)
+  
+  for(int i = 1; i < 3; ++i)
   {
-    for(int i = 1; i < 5; ++i)
-    {
-      result += texture(image, vs_texcoord + vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-      result += texture(image, vs_texcoord - vec2(tex_offset.x * i, 0.0)).rgb * weight[i];
-    }
+    vec2 offsetVec;
+
+    if (horizontal)
+      offsetVec = vec2(tex_offset.x * offset[i], 0.0);
+    else
+      offsetVec = vec2(0.0, tex_offset.y * offset[i]);
+
+    result += texture(image, vs_texcoord + offsetVec).rgb * weight[i];
+    result += texture(image, vs_texcoord - offsetVec).rgb * weight[i];
   }
-  else
-  {
-    for(int i = 1; i < 5; ++i)
-    {
-      result += texture(image, vs_texcoord + vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-      result += texture(image, vs_texcoord - vec2(0.0, tex_offset.y * i)).rgb * weight[i];
-    }
-  }
+
   fs_color = vec4(result, 1.0);
 }
